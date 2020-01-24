d923a2707fb927ef6fc88bf2932b9833d0133899
bmckenna1982 <bmckenna1982@gmail.com>
2020-01-24 11:57:29 -0500

    removed wireframe code and replced with Components

--- src/App.js
+++ src/App.js
@@ -22,65 +22,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" >
-        {/* <nav role="navigation">
-            <div>Home</div>
-            <div className="app_name">Bench Boss</div>
-            <div>Login</div>
-          </nav> */}
+      <div className="App" >        
         <Nav />
         <main role="main">
           <header role="banner">
             <h1>Guinness Hockey</h1>            
           </header>
           <section className="schedule">
-            <h2>Upcoming Games</h2>
-            {/* {schedule.map((game, index) => (
-              <GameDetail key={index} game={game} />
-            ))} */}
+            <h2>Upcoming Games</h2>            
             <SchedulePreview />
-          </section>
-          {/* <section className="schedule">
-              <h2>Schedule</h2>
-              {schedule.map((game, index) => (                
-                <div key={index} className="GameDetail">
-                <div className="GameDetail_date">
-                  <div className="month">
-                  {this.NiceMonth(game)}
-                  </div>
-                  <div className="day">
-                  {this.NiceDay(game)}
-                  </div>
-                </div>
-                <div className="GameDetail_time">
-                  {game.time}                  
-                </div>
-                <div className="GameDetail_location">
-                  {game.location}
-                </div>
-                <div className="GameDetail_opponent">
-                  {game.opponent}
-                </div>
-              </div>
-              ))}
-              
-            </section> */}
+          </section>          
           <section className="message-board">
             <h2>Message Board</h2>
             <div className="latest-message">
-              <h3>Latest message board activity</h3>
-              {/* <div>
-                <ul>
-                  <li>Contents of latest message</li>
-                  <li>Author</li>
-                  <li>Date posted</li>
-                </ul>
-                <ul>
-                  <li>Contents of latest comment on latest message</li>
-                  <li>Author</li>
-                  <li>Date posted</li>
-                </ul>
-              </div> */}
+              <h3>Latest message board activity</h3>              
               <Message />
               <Comment />
             </div>
