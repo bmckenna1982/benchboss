b3c93854a3b3ed1684faa3d4527dc96c2908cf01
bmckenna1982 <bmckenna1982@gmail.com>
2020-03-06 13:02:21 -0500

    prep for deploy

---
+++ seed.js
@@ -0,0 +1,26 @@
+const knex = require('knex')({
+  client: 'pg',
+  connection:
+    'postgres://mrixwjaeiiehqk:71f2a439f7e8dd38a5bfa63fd7a13e3f1ef0782e0aac2b9dd6627d02ec38281a@ec2-34-206-252-187.compute-1.amazonaws.com:5432/d906h1marlgr9b'
+    + '?ssl=true',
+  debug: false
+});
+
+let games = [];
+
+const schedule = require('./src/schedule-data')
+
+console.log('schedule', schedule)
+const inserts = function () {
+  const insertPromises = [];
+  schedule.forEach(function (game) {
+    insertPromises.push(knex('schedule')
+      .insert({ summary: game.summary, location: game.location, time: new Date(game.time) })
+    );
+  });
+  return Promise.all(insertPromises);
+};
+
+inserts()
+
+console.log('Inserts done');
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  // API_ENDPOINT: `https://boiling-sea-81293.herokuapp.com/api`,
-  API_ENDPOINT: `http://localhost:8000/api`,
+  API_ENDPOINT: `https://pure-bayou-67438.herokuapp.com/api`,
+  //API_ENDPOINT: `http://localhost:8000/api`,
   API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'benchboss-key',
 }
