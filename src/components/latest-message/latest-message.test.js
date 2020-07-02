2a21893491ded6fe4c8ccaa73c1799e496666a09
bmckenna1982 <bmckenna1982@gmail.com>
2020-07-02 17:15:48 -0400

    starting websockets chat

--- src/components/latest-message/latest-message.test.js
+++ src/components/latest-message/latest-message.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import LatestMessage from './latest-Message'
+import LatestMessage from './latest-message'
 
 describe('LatestMessage', () => {
 
